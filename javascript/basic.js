246e045173593a57f5587d5f84bde737aadde697
Dashmeet <dashmeet@gmail.com>
2026-06-26 11:31:22 +0530

    First Commit

---
+++ javascript/basic.js
@@ -0,0 +1,15 @@
+let age= prompt("enter your age")
+let name=prompt("enter your name")
+console.log(age)
+console.log(name)
+if(age>18){
+    alert("you are eligible "+name)   
+}
+else{
+    alert(`${name}, you are not eligible)`)
+}
+
+
+
+
+
